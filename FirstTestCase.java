454b01102fd802716e0aceb339fa95f449dab5db
Lawren <alksjdf12384@gmail.com>
2020-11-09 11:23:55 +0800

    Add files via upload

---
+++ FirstTestCase.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTestCase {
+
+     private static WebDriver driver = null;
+     public static void main(String[] args) {
+     
+
+     }
+
+
+}
